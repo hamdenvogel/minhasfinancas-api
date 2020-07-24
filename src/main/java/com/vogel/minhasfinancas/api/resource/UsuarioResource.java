699d49ac41079c477e57e7dfe0801c9fd6ddcc34
hamdenvogel <hamdenvogel@gmail.com>
2020-07-24 17:51:43 -0300

    Lancamentos controller -> finished

--- src/main/java/com/vogel/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/vogel/minhasfinancas/api/resource/UsuarioResource.java
@@ -1,8 +1,12 @@
 package com.vogel.minhasfinancas.api.resource;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +16,18 @@ import com.vogel.minhasfinancas.api.dto.UsuarioDTO;
 import com.vogel.minhasfinancas.exception.ErroAutenticacao;
 import com.vogel.minhasfinancas.exception.RegraNegocioException;
 import com.vogel.minhasfinancas.model.entity.Usuario;
+import com.vogel.minhasfinancas.service.LancamentoService;
 import com.vogel.minhasfinancas.service.UsuarioService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
 public class UsuarioResource {
 	
-	private UsuarioService service;
-	
-	public UsuarioResource(UsuarioService service) {
-		this.service = service;
-	}
+	private final UsuarioService service;
+	private final LancamentoService lancamentoService;
 	
 	
 	@PostMapping("/autenticar")
@@ -47,9 +52,19 @@ public class UsuarioResource {
 			return new ResponseEntity(usuarioSalvo,HttpStatus.CREATED);
 		} catch (RegraNegocioException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
+		}			
+	}
+	
+	@GetMapping("{id}/saldo")
+	public ResponseEntity obterSaldo(@PathVariable("id") Long id) {
+		Optional<Usuario> usuario = service.obterPorID(id);
+		if (!usuario.isPresent()) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
 		}
 		
-		
+		BigDecimal saldo = lancamentoService.obterSaldoPorUsuario(id);
+		return ResponseEntity.ok(saldo);
 	}
+	
 
 }
--- src/main/java/com/vogel/minhasfinancas/service/UsuarioService.java
+++ src/main/java/com/vogel/minhasfinancas/service/UsuarioService.java
@@ -1,9 +1,12 @@
 package com.vogel.minhasfinancas.service;
 
+import java.util.Optional;
+
 import com.vogel.minhasfinancas.model.entity.Usuario;
 
 public interface UsuarioService {
 	Usuario autenticar(String email, String senha);
 	Usuario salvarUsuario(Usuario usuario);
 	void validarEmail(String email);
+	Optional<Usuario> obterPorID(Long id);
 }
