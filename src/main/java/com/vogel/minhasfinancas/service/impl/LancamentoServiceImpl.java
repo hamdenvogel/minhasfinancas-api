792aa6095d3176eb0cff80f036f1919724bc0065
hamdenvogel <hamdenvogel@gmail.com>
2020-07-25 20:58:37 -0300

    JUnit tests finished.

--- src/main/java/com/vogel/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/vogel/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -89,7 +89,7 @@ public class LancamentoServiceImpl implements LancamentoService {
 		}
 		
 		if (lancamento.getTipo() == null) {
-			throw new RegraNegocioException("Informe um tipo de Lançamento");
+			throw new RegraNegocioException("Informe um tipo de Lançamento.");
 		}		
 		
 	}
