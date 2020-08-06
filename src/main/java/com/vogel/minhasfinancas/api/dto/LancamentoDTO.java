414ee492e3c2a395f695050fe7309189c03668af
hamdenvogel <hamdenvogel@gmail.com>
2020-08-06 00:25:40 -0300

    build 06-08-2020. First deploy on Heroku

--- src/main/java/com/vogel/minhasfinancas/api/dto/LancamentoDTO.java
+++ src/main/java/com/vogel/minhasfinancas/api/dto/LancamentoDTO.java
@@ -2,13 +2,16 @@ package com.vogel.minhasfinancas.api.dto;
 
 import java.math.BigDecimal;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LancamentoDTO {
 	private Long id;
 	private String descricao;
--- src/test/java/com/vogel/minhasfinancas/MinhasfinancasApplicationTests.java
+++ src/test/java/com/vogel/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MinhasfinancasApplicationTests {
+public class MinhasfinancasApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
